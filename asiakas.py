8c4725ca372f8cc6543d77f60e57420992e43861
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2012-09-18 02:26:55 +0200

    Simplified project structure

---
+++ asiakas.py
@@ -0,0 +1,43 @@
+from __future__ import with_statement
+import requests
+import html2text
+import os.path
+import pyfiglet
+
+def get_request(url):
+    req_get = requests.get(url)
+    cont = unicode(req_get.content, "utf-8")
+    if len(cont) == 0:
+        if req_get.status_code == 200:
+            print html2text.html2text(cont)
+        elif req_get.status_code == 404:
+            print "Error 404, Not Found!"
+        elif req_get.status_code == 500:
+            print "Error 500, Internal server error!"
+        else:
+            print html2text.html2text(cont)
+
+    # If non-empty content is detected, print it.
+    # This is to allow customised html error messages.
+    else:
+        print html2text.html2text(cont)
+
+
+def main():
+    fig = pyfiglet.Figlet(font='larry3d')
+    print fig.renderText('asiakas')
+    print "Asiakas - A tiny python text-based web browser\n".center(58)
+    print "Type 'quit' to shut down the browser.".center(58)
+
+    while True:
+        link = raw_input("URL: ")
+        if link == "quit":
+            break
+        else:
+            pass
+
+        get_request(link)
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     name='Asiakas',
     author='Robrecht De Rouck',
     author_email='Robrecht.De.Rouck@gmail.com',
-    packages=['asiakas', ],
-    package_data={'': ['ascii-art.txt']},
+    py_modules=['asiakas'],
     entry_points={
     'console_scripts': ['asiakas = asiakas:main', ],},
     description='A tiny python text-based web browser.',
     long_description=open('README.rst').read(),
     install_requires=[
-        "requests>=0.13.2", "html2text>=3.200.3",
+        "requests>=0.13.2", "html2text>=3.200.3", "pyfiglet"
     ],
     classifiers=[
         "Environment :: Console",
